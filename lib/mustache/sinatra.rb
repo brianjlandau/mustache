9fab35b9d85bbf6282b1b47d84577aaaf4b05acb
Brian Landau <brianjlandau@gmail.com>
2010-06-28 22:39:08 -0400

    Fix mustache_helpers

--- lib/mustache/sinatra.rb
+++ lib/mustache/sinatra.rb
@@ -175,10 +175,10 @@ class Mustache
     end
     
     def mustache_helpers(*helpers, &block)
-      if namespace.const_defined?(:Views)
-        staches = namespace::Views.constants.inject([]) do |staches, const_name|
-          if namespace::Views.const_get(const_name).ancestors.include?(Mustache)
-            staches << namespace::Views.const_get(const_name)
+      if self.respond_to?(:mustache) && mustache[:namespace].const_defined?(:Views)
+        staches = mustache[:namespace]::Views.constants.inject([]) do |staches, const_name|
+          if mustache[:namespace]::Views.const_get(const_name).ancestors.include?(Mustache)
+            staches << mustache[:namespace]::Views.const_get(const_name)
           end
           staches
         end
